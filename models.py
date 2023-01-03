5e0d8ae78e998f71f569bf9a09ea514b0b48ed23
ZhangWan-byte <zhang_wanzhang@foxmail.com>
2023-01-03 15:31:51 +0800

    set transformer

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 
 from dataset import *
 from utils import *
@@ -372,12 +373,11 @@ class PMA(nn.Module):
         return self.mab(self.S.repeat(X.size(0), 1, 1), X)
 
 
-# Set Transformer
-# https://github.com/juho-lee/set_transformer
 class SetTransformer(nn.Module):
     def __init__(self, dim_input, num_outputs, dim_output,
             num_inds=32, dim_hidden=128, num_heads=4, ln=False):
         super(SetTransformer, self).__init__()
+        self.embedding = nn.Embedding(len(vocab), dim_input)
         self.enc = nn.Sequential(
                 ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln),
                 ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln))
@@ -388,6 +388,7 @@ class SetTransformer(nn.Module):
                 nn.Linear(dim_hidden, dim_output))
 
     def forward(self, X):
+        X = self.embedding(X)
         return self.dec(self.enc(X))
 
 
