c9a9a43418fc1ef33bc811e8cfe5e1e64269deb4
ZhangWan-byte <zhang_wanzhang@foxmail.com>
2023-02-28 00:41:34 +0800

    commit

--- pre_train.py
+++ pre_train.py
@@ -585,7 +585,7 @@ if __name__=='__main__':
     # training
     pre_train(config=config)
     print("Results dump to: ")
-    print("./results/SAbDab/full/{}/{}/result_{}.pkl".format(config["data_type"], config["model_name"]))
+    print("./results/SAbDab/full/{}/{}/result.pkl".format(config["data_type"], config["model_name"]))
     # pickle.dump(result, open("./results/SAbDab/full/{}/{}/result_{}.pkl".format(config["data_type"], config["model_name"]), "wb"))
 
 
