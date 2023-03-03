d8c025714508043db225986d1962fd0efcc48e8d
ZhangWan-byte <zhang_wanzhang@foxmail.com>
2023-03-03 21:32:10 +0800

    commit

--- pre_train.sh
+++ pre_train.sh
@@ -2,7 +2,7 @@ declare -a arr=('masonscnn' 'lstm' 'textcnn' 'ag_fast_parapred' 'pipr' 'resppi' 
 
 for model_name in "${arr[@]}"
 do
-    echo "nohup python pre_train.py $model_name > ./logs/log_pretrain_$model_name 2>&1 &"
+    echo "python pre_train.py $model_name > ./logs/log_pretrain_$model_name 2>&1;"
 
-    nohup python pre_train.py $model_name > ./logs/log_pretrain_$model_name 2>&1 &
+    python pre_train.py $model_name > ./logs/log_pretrain_$model_name 2>&1;
 done
